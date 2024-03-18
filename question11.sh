cce6fe767d88767547cc088e073ac2e4a2b78eb9
ChuangfanZhang <163087484+ChuangfanZhang@users.noreply.github.com>
2024-03-18 20:54:10 +1100

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,6 @@
-git checkout branch2
+git checkout -b branch2
 touch file4
-code file4
 git add file4
-git commit -m "Add file4 to branch2"
-code file4
-git checkout main
+git commit -m "Add file4"
+echo "Modification in branch2" >> file4
+git checkout -
